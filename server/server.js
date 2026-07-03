34041e2cd2d03503dbf05e31bba2e6952f091046
Juraj F <smarteagle0@gmail.com>
2026-07-03 12:48:19 +0200

    initial-creation

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import http from "http";
+import { Server } from "socket.io";
+
+const httpServer = http.createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: [
+      "http://localhost:3001",
+      process.env.CLIENT_URL
+    ],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+
+httpServer.listen(PORT, () => {
+  console.log(`Socket.IO server running on ${PORT}`);
+});
